fa62e5d67724625dac1d9a47a2395b9749ed780b
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-09-12 05:36:08 -0500

    Changed route path

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./Homescreen";
 import DetailsScreen from "./DetailsScreen";
 
 // Defining a stack navigator
